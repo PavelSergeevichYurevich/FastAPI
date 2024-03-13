b9cfce346d6b24ad2422b5281a58aa21bc1a06f8
PavelSergeevichYurevich <Pasha2028@icloud.com>
2024-03-13 20:17:23 +0700

    ReadyToRedirect

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request, Body
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 import base_module
 
 app = FastAPI() # <- Создаем экземпляр класса FastAPI
@@ -13,6 +13,10 @@ def index(request: Request):
 @app.get("/login/") # <- декоратор, который обрабатывает get - запросы где маршрут
 def login(request:Request):
     return templates.TemplateResponse(request=request, name="login.html")
+
+@app.get("/register/") # <- декоратор, который обрабатывает get - запросы где маршрут
+def login(request:Request):
+    return templates.TemplateResponse(request=request, name="register.html")
  
 @app.get("/users/", response_class = HTMLResponse) # <- декоратор, который обрабатывает get - запросы где маршрут
 def get_users_page(request:Request):
@@ -32,6 +36,18 @@ def add_us(data = Body()):
     user = Person(email = new_email, password = new_password)
     db.add(user)
     db.commit()
+    
+@app.post("/check/")
+def check_us(data = Body()):
+    db, Person = base_module.create_open_base()
+    new_email = data["mail"]
+    new_password = data["pass"]
+    users = db.query(Person).all()
+    for user in users:
+        if (user.email == new_email) and (user.password == new_password):
+            print('ok')
+    
+    
 
 
 # uvicorn main:app --reload
