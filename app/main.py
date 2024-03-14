29be401bf7e260ef9bafac86df64bc399db11489
PavelSergeevichYurevich <Pasha2028@icloud.com>
2024-03-14 12:36:03 +0700

    Перенаправляет но не работает else

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Body
+from fastapi import FastAPI, Request, Body, status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 import base_module
@@ -7,19 +7,23 @@ app = FastAPI() # <- Создаем экземпляр класса FastAPI
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/") # <- декоратор, который обрабатывает get - запросы где маршрут
-def index(request: Request):
+async def index(request: Request):
     return templates.TemplateResponse(request=request, name='index.html')
 
 @app.get("/login/") # <- декоратор, который обрабатывает get - запросы где маршрут
-def login(request:Request):
+async def login(request:Request):
     return templates.TemplateResponse(request=request, name="login.html")
 
 @app.get("/register/") # <- декоратор, который обрабатывает get - запросы где маршрут
-def login(request:Request):
+async def login(request:Request):
     return templates.TemplateResponse(request=request, name="register.html")
+
+@app.get("/enter/") # <- декоратор, который обрабатывает get - запросы где маршрут
+async def enter(request:Request):
+    return templates.TemplateResponse("enter.html", {"request": request})
  
 @app.get("/users/", response_class = HTMLResponse) # <- декоратор, который обрабатывает get - запросы где маршрут
-def get_users_page(request:Request):
+async def get_users_page(request:Request):
     db, Person = base_module.create_open_base()
     users = db.query(Person).all()
     context = {}
@@ -29,23 +33,26 @@ def get_users_page(request:Request):
     return templates.TemplateResponse("users.html", {"request": request, "context": context})
 
 @app.post("/rec/")
-def add_us(data = Body()):
+async def add_us(data = Body()):
     db, Person = base_module.create_open_base()
     new_email = data["mail"]
     new_password = data["pass"]
     user = Person(email = new_email, password = new_password)
     db.add(user)
     db.commit()
+    return RedirectResponse(url="/login/", status_code=status.HTTP_302_FOUND)
     
 @app.post("/check/")
-def check_us(data = Body()):
+async def check_us(data = Body()):
     db, Person = base_module.create_open_base()
     new_email = data["mail"]
     new_password = data["pass"]
     users = db.query(Person).all()
     for user in users:
         if (user.email == new_email) and (user.password == new_password):
-            print('ok')
+            return RedirectResponse(url="/enter/", status_code=status.HTTP_302_FOUND)
+        """  else:
+            return 'Такого пользователя нет. Зарегистрируйтесь.' """
     
     
 
